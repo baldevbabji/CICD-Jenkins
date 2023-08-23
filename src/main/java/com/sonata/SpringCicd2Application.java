cb1642c652249b453056380d486ec38043eff3c9
baldevbabji2000 <baldevbabji2000@gmail.com>
2023-08-23 15:00:03 +0530

    Modified SpringBootServletInitializer

--- src/main/java/com/sonata/SpringCicd2Application.java
+++ src/main/java/com/sonata/SpringCicd2Application.java
@@ -2,12 +2,19 @@ package com.sonata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringCicd2Application {
+public class SpringCicd2Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCicd2Application.class, args);
 	}
 
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringCicd2Application.class);
+	}
 }
